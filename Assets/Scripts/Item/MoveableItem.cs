8c52348c4b19b7687fe22be331c057b9a7855ec8
benjamin.brodolf@students.fhnw.ch <skaten88>
2020-11-15 16:30:49 +0100

    add Level Scene add Rock and Wood Item

---
+++ Assets/Scripts/Item/MoveableItem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableItem : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private bool playerTrigger;
+    
+    public bool IsPlayerInTriggerToItem() => playerTrigger;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Trigger with Player !");
+            playerTrigger = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerTrigger = false;
+        }
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PushItemInDirection(Vector3 newDirection)
+    {
+        rigidbody.MovePosition(transform.position + newDirection);
+    }
+}
