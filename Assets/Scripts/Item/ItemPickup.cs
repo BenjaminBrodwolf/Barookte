c818b1f404fc22360fd75126cbe47187dc7ec67b
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2020-11-30 19:24:51 +0100

    pickup item

---
+++ Assets/Scripts/Item/ItemPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    private bool itemIsPicekdUp = false;
+    private void OnTriggerEnter(Collider other)
+    {
+   
+        if (!itemIsPicekdUp && other.CompareTag("Player"))
+        {
+            Debug.Log("Item picked up");
+            Debug.Log(other.gameObject); // player(clone) Gameobject
+            itemIsPicekdUp = true;
+
+            gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            // disable the Colliders
+            foreach (var boxCollider in gameObject.GetComponents<BoxCollider>())
+            {
+                boxCollider.enabled = false;
+            }
+
+            // Item add as ChildObject to Player
+            gameObject.transform.parent = other.transform;
+
+
+            // Destroy(gameObject);
+        }
+    }
+}
