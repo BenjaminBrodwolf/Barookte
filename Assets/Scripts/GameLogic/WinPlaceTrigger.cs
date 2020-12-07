88ef93e342ba067ff0d302968c4ea9fb0a914bb0
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2020-12-07 22:32:56 +0100

    missed FILES to commit

---
+++ Assets/Scripts/GameLogic/WinPlaceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPlaceTrigger : MonoBehaviour
+{
+    private GameManager gameManagerScript;
+    void Start()
+    {
+        gameManagerScript =  GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+    }
+
+    // Player on bridge -> WIN
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Debug.Log("Trigger with Player !");
+            gameManagerScript.StartBlackout();
+            gameManagerScript.WaitForSecondsFunctionAndRestart(3);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject fadeBlackout;
+
+    void Start()
+    {
+       StartBlackout();
+    }
+
+ 
+
+    
+
+    public void EndBlackout() => fadeBlackout.SetActive(false);
+    
+    public void StartBlackout() => fadeBlackout.SetActive(true);
+    
+}
