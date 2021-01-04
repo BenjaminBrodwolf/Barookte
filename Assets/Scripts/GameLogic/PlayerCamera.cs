ef00fa67cf5ffd949ac005a9f1a57be41ef2e7eb
Benjamin Brodwolf <benjamin.brodolf@students.fhnw.ch>
2021-01-04 18:45:42 +0100

    Level kniffel

---
+++ Assets/Scripts/GameLogic/PlayerCamera.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private static PlayerCamera _control;
+
+    private void Awake()
+    {
+        
+        if (_control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _control = this;
+        }
+        else if (_control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        if (currentScene.name == "LevelBuilder")
+        {
+            Debug.Log("Deactive Player Camera");
+            gameObject.SetActive(false); 
+        }
+    }
+}
---
+++ Assets/Scripts/LevelBuildingSystem/TestPlayLevel.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestPlayLevel : MonoBehaviour
+{
+    // Type in the name of the Scene you would like to load in the Inspector
+    public string testPlaySceneName;
+
+    // Assign your GameObject you want to move Scene in the Inspector
+    public GameObject buildetLevelGameObject;
+    void Update()
+    {
+        // Press the space key to add the Scene additively and move the GameObject to that Scene
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Start TestPlayLevel");
+  
+            StartCoroutine(LoadTestPlayLevelAsync());
+        }
+    }
+
+    IEnumerator LoadTestPlayLevelAsync()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(testPlaySceneName, LoadSceneMode.Single);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(buildetLevelGameObject, SceneManager.GetSceneByName(testPlaySceneName));
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
+}
